8aa937c60422a066b61d5c74c9fd0c6ec74c1edd
aditya-rai-5 <aditya5748rai@gmail.com>
2025-05-27 09:29:19 +0530

    DSA

--- CF_strin_mirror.cpp
+++ CF_strin_mirror.cpp
@@ -1,3 +1,13 @@
 // https://codeforces.com/contest/2044/problem/B
 
 #include<iostream>
+#include<string>
+
+using namespace std;
+
+string mirror(string s){
+    int len=s.length();
+    for(int i=0;i<len;i++){
+        
+    }
+}
