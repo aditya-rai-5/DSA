8e84f64e09193a2354bf49bb2444a8a234e9d58a
aditya-rai-5 <aditya5748rai@gmail.com>
2025-05-27 09:57:00 +0530

    DSA

---
+++ CF_max_monkey_seats.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int m,a,b,c;
+    cin>>m>>a>>b>>c;
+    int ans=0,rem=0;
+    ans+=min(m,a);rem+=m-min(m,a);
+    ans+=min(m,b);rem+=m-min(m,b);
+    ans+=min(rem,c);
+    cout<<ans<<endl;
+}
+        
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        solve();
+    }
+}
