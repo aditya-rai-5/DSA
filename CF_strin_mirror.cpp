973a0f4306f99cc60c700e05c4d7809331cfbc34
aditya-rai-5 <aditya5748rai@gmail.com>
2025-05-27 09:29:44 +0530

    DSA

--- CF_strin_mirror.cpp
+++ CF_strin_mirror.cpp
@@ -2,12 +2,31 @@
 
 #include<iostream>
 #include<string>
-
+#include<algorithm>
 using namespace std;
 
 string mirror(string s){
     int len=s.length();
-    for(int i=0;i<len;i++){
-        
+    for(char &c:s){
+        if(c=='p'){
+            c='q';
+        }
+        else if(c=='q'){
+            c='p';
+        }
     }
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        string s;
+        cin>>s;
+        cout<<mirror(s)<<endl;
+    }
+
+    return 0;
 }
